6d18ad6010c82a211a9b701d4f9553ccb1286493
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-13 17:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function insert_Row() {
-    //Write your code here
-  
-  
-}
+        function insert_Row(){
+         
+            table=document.getElementById('sampleTable')
+             let newRow=document.createElement('tr')
+            let newCell = document.createElement('td'); 
+            let cell2=document.createElement('td')
+            newCell.textContent='New Cell1'
+            cell2.textContent='New Cell2'
+            newRow.appendChild(newCell)
+            newRow.appendChild(cell2)
+            table.prepend(newRow);
+          
+                
+                    }
